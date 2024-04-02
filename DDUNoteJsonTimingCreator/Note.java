6faa6226ef9a00a8ba24f2017bab292dff9071c0
devinbrantley <164040678+devinbrantley@users.noreply.github.com>
2024-04-02 10:42:15 -0500

    Add files via upload

---
+++ DDUNoteJsonTimingCreator/Note.java
@@ -0,0 +1,10 @@
+package DDUstuff;
+
+public class Note{
+    public int frameoffset;
+    public float positionX;
+    public Note(int f, float p){
+        this.frameoffset = f;
+        this.positionX = p;
+    }
+}
