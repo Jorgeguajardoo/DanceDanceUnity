e3882256c89b4a1b74778cf020ac9027c9e87f5b
devinbrantley <164040678+devinbrantley@users.noreply.github.com>
2024-04-26 12:54:01 -0500

    better versions and offset tester added

--- DDUNoteJsonTimingCreator/OffsetTesterCreator.java
+++ DDUNoteJsonTimingCreator/OffsetTesterCreator.java
@@ -2,7 +2,8 @@ package DDUNoteJsonTimingCreator;
 import java.util.*;
 import java.io.*;
 
-public class SongNoteJsonCreatorBeatsInsteadOfFrames{
+
+public class OffsetTesterCreator{
     public static void main(String[] args) throws Exception{
         Scanner scan = new Scanner(System.in);
         System.out.println("CREATOR FOR JSON FILES\nOutputted to terminal, please paste into notepad file");
@@ -10,30 +11,30 @@ public class SongNoteJsonCreatorBeatsInsteadOfFrames{
         System.out.println("Please Enter Song BPM:");
         double bpm = scan.nextDouble();
         System.out.println("Please enter start of song frame offset");
-        int frameoffset = scan.nextInt();
+        int frameoffset = scan.nextInt(); scan.nextLine();
         
-        while(true){
-            System.out.println("Enter note formatted \"doubleBeatNumber,floatoffsetX\"");
-            System.out.println("Type \"FINISH\" to finish and print the json file string");
-            System.out.println("Type \"UNDO\" to remove the previous note");
-            if(list.size()>0){
-                System.out.println("Previous note info: " + list.get(list.size()-1).beat + " beat");
-                System.out.println("                    " + list.get(list.size()-1).positionX + " offset");
-            }
-            String[] arr = scan.nextLine().split(",");
-            if(arr[0].equals("FINISH")){
-                break;
-            }
-            if(arr[0].equals("UNDO")){
-                list.remove(list.size()-1);
-            }
-            if(arr.length!=2){
-                System.out.println("\fInvalid input.");
-                continue;
-            }
-            double beat = (Double.parseDouble(arr[0]));
-            list.add(new Note((int)((beat-1.0)*(60.0/bpm)*100+frameoffset), Float.parseFloat(arr[1]), beat));
-            System.out.print("\f");
+        for(int beat = 0; beat<300; beat++){
+            // System.out.println("Enter note formatted \"doubleBeatNumber,floatoffsetX\"");
+            // System.out.println("Type \"FINISH\" to finish and print the json file string");
+            // System.out.println("Type \"UNDO\" to remove the previous note");
+            // if(list.size()>0){
+                // System.out.println("Previous note info: " + list.get(list.size()-1).beat + " beat");
+                // System.out.println("                    " + list.get(list.size()-1).positionX + " offset");
+            // }
+            // String[] arr = scan.nextLine().split(",");
+            // if(arr[0].equals("FINISH")){
+                // break;
+            // }
+            // if(arr[0].equals("UNDO")){
+                // list.remove(list.size()-1);
+            // }
+            // if(arr.length!=2){
+                // System.out.println("\fInvalid input.");
+                // continue;
+            // }
+            // double beat = (Double.parseDouble(arr[0]));
+            list.add(new Note((int)((beat-1.0)*(60.0/bpm)*100+frameoffset), 0, beat));
+            // System.out.print("\f");
         }
         StringBuilder str = new StringBuilder("{\n  \"noteArray\": [\n");
         for(Note n : list){
